5de1dcb1c9a145e43664f9584bee80d72919a537
Esteban Vazquez <esteban.vazquez@techbrainscorp.com>
2024-03-26 15:44:00 -0300

    finished desktop main content; starting desktop header

--- app/page.js
+++ app/page.js
@@ -2,31 +2,33 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='container mx-auto px-4 lg:px-[10.25rem] my-[7.5rem] lg:my-[11.25rem] space-y-8'>
-      <section className='space-y-6'>
+    <main className='px-4 lg:px-[10.25rem] my-[7.5rem] lg:my-[11.25rem] grid gap-8 lg:gap-[1.875rem]'>
+      <section className='max-md:space-y-6 lg:flex lg:gap-[1.875rem]'>
         <Image
           src='/image-grid-1.jpg'
           alt='Image of art gallery'
           width={635}
           height={400}
-          className='max-md:h-80 max-md:w-auto max-md:object-cover'
+          className='max-md:h-80 max-md:w-auto max-md:object-cover lg:order-last'
         />
-        <h2 className='text-balance max-md:whitespace-pre-line'>
-          YOUR DAY {'\n'} AT THE GALLERY
-        </h2>
-        <p>
-          Wander through our distinct collections and find new insights about
-          our artists. Dive into the details of their creative process.
-        </p>
+        <div className='space-y-5 lg:space-y-8 lg:pr-[5.9375rem] lg:my-[3.75rem]'>
+          <h2 className='max-md:whitespace-pre-line lg:text-balance'>
+            YOUR DAY {'\n'} AT THE GALLERY
+          </h2>
+          <p>
+            Wander through our distinct collections and find new insights about
+            our artists. Dive into the details of their creative process.
+          </p>
+        </div>
       </section>
 
-      <section className='space-y-4'>
+      <section className='grid gap-4 lg:gap-[1.875rem] lg:grid-cols-[auto_1fr] lg:grid-rows-[auto_1fr]'>
         <Image
           src='/image-grid-2.jpg'
           alt='Image of art gallery'
           width={635}
           height={720}
-          className='max-md:h-[30rem] max-md:w-auto max-md:object-cover'
+          className='max-md:h-[30rem] max-md:w-auto max-md:object-cover lg:row-span-2'
         />
         <Image
           src='/image-grid-3.jpg'
@@ -35,7 +37,7 @@ export default function Home() {
           height={313}
           className='max-md:h-[12.5rem] max-md:w-full max-md:object-cover'
         />
-        <div className='bg-black px-6 py-12 space-y-6'>
+        <div className='px-6 py-12 space-y-6 bg-black'>
           <h2 className='text-white'>COME &amp; BE INSPIRED</h2>
           <p className='text-white'>
             We’re excited to welcome you to our gallery and see how our
